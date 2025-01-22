369f4e22a6c98c376ded2e7f7e224aaaaaeec922
Sonia487 <sonia.luo.ys@gmail.com>
2025-01-22 15:07:49 +0800

    Add files via upload

--- js/dropdownmenu.js
+++ js/dropdownmenu.js
@@ -4,7 +4,7 @@ let tourData = {};
 let categories = [];
 
 // 1. 載入所有類別資料
-fetch('/Sonia487.github.io/js/Category.json')
+fetch('https://sonia487.github.io/js/Category.json')
   .then(response => response.json())
   .then(data => {
     categories = data.categories;
